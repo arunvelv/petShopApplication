5d2ebb26e55434240dcc1c2f5ca7c8353cf255e2
Roopachinnam1408 <chinnam.roopachandrika@capgemini.com>
2025-01-03 16:35:31 +0530

    added admin petfoods

--- Frontend/PetShopApplication/src/AdminModule/components/admin-pet-foods/admin-pet-foods.component.spec.ts
+++ Frontend/PetShopApplication/src/AdminModule/components/admin-pet-foods/admin-pet-foods.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PetFoodsComponent } from '../../../UserModule/components/pet-foods/pet-foods.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AdminPetFoodsComponent } from './admin-pet-foods.component';
 
 describe('AdminPetFoodsComponent', () => {
   let component: AdminPetFoodsComponent;
   let fixture: ComponentFixture<AdminPetFoodsComponent>;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
