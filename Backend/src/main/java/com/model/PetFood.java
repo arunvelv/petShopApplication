b6fc1a9e46683ea5f18339ac3e49fde2935cc465
Roopachinnam1408 <chinnam.roopachandrika@capgemini.com>
2025-01-03 16:58:17 +0530

    updated petfood java files

--- Backend/src/main/java/com/model/PetFood.java
+++ Backend/src/main/java/com/model/PetFood.java
@@ -1,9 +1,8 @@
 package com.model;
- 
+
 import java.util.List;
- 
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -11,131 +10,133 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
- 
+
 @Entity
 @Table(name = "pet_food")
 public class PetFood
 {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="food_id", nullable=false)
+	@GeneratedValue
+	@Column(name="food_id")
 	private int foodId;
 	
-//	@NotNull
+	@NotNull
 	@Size(max=255)
-	@Column(name="name", nullable=false)
+	@Column(name="name")
 	private String name;
 	
 	@Size(max = 255)
-	@Column(name = "brand", nullable=false)
+	@Column(name = "brand")
 	private String brand;
 	
 	@Size(max = 255)
-    @Column(name = "type",  nullable=false)
+    @Column(name = "type")
 	private String type;
 	
-//	@NotNull
-	@Column(name = "quantity",  nullable=false)
+	@NotNull
+	@Column(name = "quantity")
 	private int quantity;
 	
-	@Column(name = "price",  nullable=false)
+	@NotNull
+	@Column(name = "price")
 	private float price;
-		
-	@OneToMany(mappedBy = "pet_food", cascade = CascadeType.ALL)
+	
+	@Size(max = 255)
+	@Column(name = "image_url")
+	private String imageURL;
+	
+	// Relationship with Pets
+	
+	@ManyToMany(mappedBy = "petFoodList", cascade = CascadeType.PERSIST)
 	@JsonIgnore
-    private List<Pets> pets;
+	private List<Pets> petfood;
 	
-	public PetFood() {}
-	
-	
- 
-	public PetFood(int foodId, @NotNull @Size(max = 255) String name, @Size(max = 255) String brand,
-			@Size(max = 255) String type, @NotNull int quantity, @NotNull float price, List<Pets> pets) {
-		super();
-		this.foodId = foodId;
-		this.name = name;
-		this.brand = brand;
-		this.type = type;
-		this.quantity = quantity;
-		this.price = price;
-		this.pets = pets;
+	public PetFood()
+	{
+		
 	}
 
+	public String getImageURL() {
+		return imageURL;
+	}
 
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
 
 	public int getFoodId() {
 		return foodId;
 	}
- 
+
 	public void setFoodId(int foodId) {
 		this.foodId = foodId;
 	}
- 
+
 	public String getName() {
 		return name;
 	}
- 
+
 	public void setName(String name) {
 		this.name = name;
 	}
- 
+
 	public String getBrand() {
 		return brand;
 	}
- 
+
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
- 
+
 	public String getType() {
 		return type;
 	}
- 
+
 	public void setType(String type) {
 		this.type = type;
 	}
- 
+
 	public int getQuantity() {
 		return quantity;
 	}
- 
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
- 
+
 	public float getPrice() {
 		return price;
 	}
- 
+
 	public void setPrice(float price) {
 		this.price = price;
 	}
 
-	public List<Pets> getPets() {
-		return pets;
+	public List<Pets> getPetfood() {
+		return petfood;
 	}
 
-	public void setPets(List<Pets> pets) {
-		this.pets = pets;
+	public void setPetfood(List<Pets> petfood) {
+		this.petfood = petfood;
 	}
+
+	@Override
+	public String toString() {
+		return "PetFood [foodId=" + foodId + ", name=" + name + ", brand=" + brand + ", type=" + type + ", quantity="
+				+ quantity + ", price=" + price + ", petfood=" + petfood + "]";
+	}
+
+	
+
+	
+
 	
 	
 	
+
 
 }
- 
-	
- 
-	
- 
-	
-	
-	
- 
- 
- 
- 
